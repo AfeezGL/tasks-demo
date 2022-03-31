edaeb5157b4e5bb0d1d09664fb7afb3fca526dab
Afeez G Lawal <afeezlg@gmail.com>
2022-03-31 01:17:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Tasks from "./pages/Tasks";
+import NewTask from "./pages/NewTask";
+import SingleTask from "./pages/SingleTask";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Tasks />} />
+        <Route path="/add" element={<NewTask />} />
+        <Route path="/:id" element={<SingleTask />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
